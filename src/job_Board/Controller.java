622b348182e0ac60858c650caea0d049ccb6fead
dePazAurelie <30385374+dePazAurelie@users.noreply.github.com>
2017-09-17 10:57:08 +0200

    Gui terminé ! Juste un petit problème avec le tableau.

--- src/job_Board/Controller.java
+++ src/job_Board/Controller.java
@@ -1,6 +1,5 @@
 package job_Board;
 
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -9,8 +8,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
-
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
@@ -123,7 +120,7 @@ public class Controller implements Initializable {
         contactEmailLabel.setText(advertisement.getContactEmail());
     }
 
-    void loadTableView() {
+    private void loadTableView() {
         for ( int i = 0; i < tableView.getItems().size(); i++) {
             tableView.getItems().clear();
         }
--- src/job_Board/editWindowController.java
+++ src/job_Board/editWindowController.java
@@ -65,7 +65,6 @@ public class editWindowController implements Initializable {
         advertisement.setSalary(salaryTextField.getText());
         advertisement.setContactName(contactNameTextField.getText());
         advertisement.setContactEmail(contactEmailTextField.getText());
-
     }
 
     void initData(Advertisement advertisement) {
