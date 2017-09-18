a5a2b9a0b501b601abb744c4d18b4d1656fc7e58
dePazAurelie <30385374+dePazAurelie@users.noreply.github.com>
2017-09-18 15:53:25 +0200

    final

--- webPages/js/jobBoard.js
+++ webPages/js/jobBoard.js
@@ -13,3 +13,9 @@ $(document).ready(function(){
         $('.collapsible').collapsible();
     });
 });
+
+
+  $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
